6c070177d033d00da74f5851da7d1a4c837f085e
Champ <champsrw49@gmail.com>
2024-12-20 21:26:50 +0700

    Lap10complete

--- lab10_2.cpp
+++ lab10_2.cpp
@@ -4,19 +4,18 @@
 using namespace std;
 
 int main (){
-	ifstream source;
-	ofstream dest;
-	source.open("cheerbook.txt") ;
-	dest.open("cheerbook_copy.txt");
-	string textline;
-	while (getline(source,textline)){
-		cout << textline << '\n';
-	}
-	cout << "-------------------- HA!! ---------------------" << '\n';
-	//กรอก Code เฉพาะส่วนนี้ ให้สอดคล้องกับโจทย์และ Code ส่วนอื่น ๆ 
-	//โดยห้ามแก้ไข Code ในส่วนอื่น ๆ
+    ifstream source;
+    ofstream dest;
+    source.open("cheerbook.txt");
+    dest.open("cheerbook_copy.txt");
 
+    dest << "-------------------- BOOM ---------------------" << '\n';
+    string textline;
+    while (getline(source, textline)) {
+        dest << textline << '\n';
+    }
+     dest << "-------------------- HA!! ---------------------" << '\n';
     source.close();
     dest.close();
-	return 0;
+    return 0;
 }
--- lab10_3.cpp
+++ lab10_3.cpp
@@ -1,3 +1,42 @@
-//cout << "Number of data = ";
-//cout << "Mean = ";
-//cout << "Standard deviation = ";
+#include <iostream>
+#include <iomanip>
+#include <cmath>
+#include <string>
+#include <cstdlib>
+#include <fstream>
+using namespace std;
+
+int main()
+{
+    int count = 0;
+    float sum = 0;
+    float sum_of_square = 0;
+    string textline;
+
+
+    ifstream source("score.txt");
+
+
+    while (getline(source, textline))
+    {
+        float score = atof(textline.c_str()); 
+        sum += score;
+        sum_of_square += score * score;
+        count++;
+    }
+
+
+    float mean = sum / count;
+
+
+    float variance = (sum_of_square / count) - (mean * mean);
+    float standard_deviation = sqrt(variance);
+
+
+    cout << "Number of data = " << count << "\n";
+    cout << setprecision(3);
+    cout << "Mean = " << mean << "\n";
+    cout << "Standard deviation = " << standard_deviation << "\n";
+
+    return 0;
+}
